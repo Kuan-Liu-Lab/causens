9718988f5c0af2cba807d2d8c45bd4ffd53ad4df
Larry Dong <larry.dong@mail.utoronto.ca>
2024-02-08 15:39:50 -0500

    Justifying c1, c0 choices in simulation
    Co-authored-by: Yushu Zou <yushu.zou@mail.utoronto.ca>

--- tests/testthat/example_analysis.R
+++ tests/testthat/example_analysis.R
@@ -1,20 +1,39 @@
+# Analysis with all confounders (X, U) being measured. Later on, we assume that
+# U could be missing.
+
+trt_effect <- 1
+data <- simulate_data(N = 10000, alpha_uz = 1, beta_uy = 1,
+                      treatment_effects = trt_effect)
+
+# Implementing PSweight's utility from scratch...
+
+trt_model <- glm(Z ~ X.1 + X.2 + X.3 + U, family = binomial(), data = data)
+e <- predict(trt_model, type = "response")
+weights <- (1 / e) * data$Z + (1 / (1 - e)) * (1 - data$Z)
+Y1_hat <- sum((data$Y * weights)[data$Z == 1]) / sum(weights[data$Z == 1])
+Y0_hat <- sum((data$Y * weights)[data$Z == 0]) / sum(weights[data$Z == 0])
+
+ate_correct_estimate <- Y1_hat - Y0_hat
+
 library(PSweight)
 
-# Analysis with "missing confounder" U
+# Using PSweight
 
-data <- simulate_data(N = 1000, alpha_uz = 2, beta_uy = 2)
-ate_correct <- PSweight( # adjusting for "unmeasured" confounder
-  ps.formula = Z ~ 1 + X.1 + X.2 + X.3 + U,
+ate_correct <- PSweight( # adjusts for confounder U
+  ps.formula = Z ~ X.1 + X.2 + X.3 + U,
   yname = "Y",
   family = "binomial",
   weight = "IPW",
   ps.method = "glm",
-  data = data,
+  data = data
 )
-head(data)
-summary(ate_correct)$estimates[1]
 
-ate_incorrect <- PSweight( # does *not* adjust for missing confounder
+test_that("Adjusting for U yields correct ATE estimates", {
+  expect_equal(ate_correct_estimate, summary(ate_correct)$estimates[1])
+  expect_equal(summary(ate_correct)$estimates[1], trt_effect, tolerance = 0.05)
+})
+
+ate_incorrect <- PSweight( # adjusts for confounder U
   ps.formula = Z ~ X.1 + X.2 + X.3,
   yname = "Y",
   family = "binomial",
@@ -23,44 +42,44 @@ ate_incorrect <- PSweight( # does *not* adjust for missing confounder
   data = data
 )
 
-summary(ate_incorrect)$estimates[1] # does not work
+test_that("Not adjusting for U yields incorrect ATE estimates", {
+  expect_false(summary(ate_incorrect)$estimates[1] == trt_effect)
+})
 
 # What if U were missing?
 # First, consider that we have potential outcomes Y(1) and Y(0)
 # which, by the fundamental problem of causal inference, we don't
 
-run_simulation <- function(seed) {
+run_simulation <- function(seed, c1, c0) {
 
-  data <- simulate_data(N = 1000, alpha_uz = 2, beta_uy = 2, seed = seed)
-
-  X <- data[, c("X.1", "X.2", "X.3", "U")]
-
-  model1 <- lm(Y1 ~ Z + X.1 + X.2 + X.3 + U, data = data)
-  model0 <- lm(Y0 ~ Z + X.1 + X.2 + X.3 + U, data = data)
-
-  Y1_hat <- predict(model1, newdata = data.frame(Z = 1, X))
-  Y0_hat <- predict(model0, newdata = data.frame(Z = 0, X))
-
-  true_c1 <- mean(Y1_hat[data$Z == 1]) - mean(Y1_hat[data$Z == 0])
-  true_c0 <- mean(Y0_hat[data$Z == 1]) - mean(Y0_hat[data$Z == 0])
-
-  c(true_c1, true_c0)
+  data <- simulate_data(N = 1000, alpha_uz = 1, beta_uy = 1,
+                        seed = seed, treatment_effects = trt_effect)
 
   trt_model <- glm(Z ~ X.1 + X.2 + X.3, family = binomial(), data = data)
   e <- predict(trt_model, type = "response") # propensity score
 
   weights <- 1 / ifelse(data$Z, e, 1 - e)
-  Y_sf <- data$Y + (-1)**(data$Z == 1) * abs(data$Z - predict(trt_model, type = "response")) * ifelse(data$Z == 1, true_c1, true_c0)
+  Y_sf <- data$Y + (-1)**(data$Z == 1) * abs(data$Z - e) * ifelse(data$Z, c1, c0)
 
-  estimated_ate <- sum((Y_sf * weights)[data$Z == 1]) / sum(weights[data$Z == 1]) - sum((Y_sf * weights)[data$Z == 0]) / sum(weights[data$Z == 0])
+  Y1_sf <- sum((Y_sf * weights)[data$Z == 1]) / sum(weights[data$Z == 1])
+  Y0_sf <- sum((Y_sf * weights)[data$Z == 0]) / sum(weights[data$Z == 0])
+
+  estimated_ate <- Y1_sf - Y0_sf
 
   return(estimated_ate)
 }
 
-# run simulation, retrieve ATE after adjusting for unmeasured confounding
+# Because alpha_uz > 0 and beta_uy > 0, treated individuals are more likely to
+# have a better outcome, i.e. both potential outcomes Y(1) and Y(0). Hence, we
+# need c1 > 0 and c0 > 0.
+# Here, c1 = 0.25 and c0 = 0.25 seem to be most valid choices. It is hard to
+# determine the exact numerical value of c1 and c0, but their magnitude can be.
 simulated_ates <- c()
 
 for (iter in 1:1000){
-  simulated_ates <- c(simulated_ates, run_simulation(iter))
+  simulated_ates <- c(simulated_ates, run_simulation(iter, 0.25, 0.25))
 }
-mean(simulated_ates)
+
+test_that("Simulation retrieves ATE with 'correct' c1, c0 values", {
+  expect_equal(mean(simulated_ates), trt_effect, tolerance = 0.01)
+})
