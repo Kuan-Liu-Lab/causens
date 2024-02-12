46a11926d067e62d115cd5cf364104a8dc0168d8
Larry Dong <larry.dong@mail.utoronto.ca>
2024-02-12 18:45:48 -0500

    Fix linting and causens function coherence

--- R/causens.R
+++ R/causens.R
@@ -20,12 +20,12 @@ causens <- function(trt_model, data, exposure, outcome, c1 = 0.5, c0 = 0.3, s1 =
   e <- predict(trt_model, type = "response")
 
   # Calculate the Average Treatment Effect
-  weights <- 1 / ifelse(data$Z, e, 1 - e)
-  Y_sf <- data$Y + (-1)**(data$Z == 1) * abs(data$Z - e) * ifelse(data$Z, c1, c0)
+  weights <- 1 / ifelse(z == 1, e, 1 - e)
+  Y_sf <- y + (-1)**(z == 1) * abs(z - e) * ifelse(z, c1, c0)
 
   # Potential outcomes corrected w.r.t. sensitivity function
-  Y1_sf <- sum((Y_sf * weights)[data$Z == 1]) / sum(weights[data$Z == 1])
-  Y0_sf <- sum((Y_sf * weights)[data$Z == 0]) / sum(weights[data$Z == 0])
+  Y1_sf <- sum((Y_sf * weights)[z == 1]) / sum(weights[z == 1])
+  Y0_sf <- sum((Y_sf * weights)[z == 0]) / sum(weights[z == 0])
 
   estimated_ate <- Y1_sf - Y0_sf
 
