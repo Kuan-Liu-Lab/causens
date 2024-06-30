168300020b9c1d80be9088e0a297c760eb5f0b36
Larry Dong <larry.dong@mail.utoronto.ca>
2024-06-29 23:11:24 -0400

    Use only null test

--- tests/testthat/test_causens_binary.R
+++ tests/testthat/test_causens_binary.R
@@ -1,11 +1,6 @@
 parameters <- list(
-  list(trt_effect = 1, alpha_uz = 1, beta_uy = 1),
-  list(trt_effect = 2, alpha_uz = 0.5, beta_uy = 0.2),
-  list(trt_effect = 1, alpha_uz = -0.5, beta_uy = 0.2),
-  list(trt_effect = 1, alpha_uz = 0.5, beta_uy = -0.2),
-  list(trt_effect = 1, alpha_uz = -0.5, beta_uy = -0.2)
+  list(alpha_uz = 0, beta_uy = 0, trt_effect = 1)
 )
-
 
 for (params in parameters) {
 
@@ -26,26 +21,14 @@ for (params in parameters) {
 
     # On retrieving c1, c0 according to our data-generating mechanisms
 
-    y1_z1_model <- glm(Y1 ~ U + X.1 + X.2 + X.3, data = subset(data, data$Z == 1), family = binomial())
-    y1_z0_model <- glm(Y1 ~ U + X.1 + X.2 + X.3, data = subset(data, data$Z == 0), family = binomial())
-    y1_z1 <- predict(y1_z1_model, type = "response", newdata = data)
-    y1_z0 <- predict(y1_z0_model, type = "response", newdata = data)
-    c1 <- log(mean(y1_z1) / mean(y1_z0))
-
-    y0_z1_model <- glm(Y0 ~ U + X.1 + X.2 + X.3, data = subset(data, data$Z == 1), family = binomial())
-    y0_z0_model <- glm(Y0 ~ U + X.1 + X.2 + X.3, data = subset(data, data$Z == 0), family = binomial())
-    y0_z1 <- predict(y0_z1_model, type = "response", newdata = data)
-    y0_z0 <- predict(y0_z0_model, type = "response", newdata = data)
-    c0 <- log(mean(y0_z1) / mean(y0_z0))
-
     trt_model <- glm(Z ~ X.1 + X.2 + X.3, family = binomial(), data = data)
 
-    return(causens(trt_model, data, "Z", "Y", method = "Li", c1 = c1, c0 = c0))
+    return(causens(trt_model, data, "Z", "Y", method = "Li", c1 = 0, c0 = 0))
   }
 
   simulated_ates <- unlist(lapply(1:1000, run_simulation))
 
   test_that("Simulation retrieves ATE with 'correct' c1, c0 values", {
-    expect_true(abs(mean(simulated_ates) - true_risk_diff) < 0.1)
+    expect_true(abs(mean(simulated_ates) - true_risk_diff) < 0.05)
   })
 }
