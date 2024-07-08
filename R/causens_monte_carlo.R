924dd573b9a8ea9d368323439e5222485e973b2a
larryshamalama <larry.dong@mail.utoronto.ca>
2024-07-08 19:53:32 -0400

    Adding Monte Carlo sensitivity analysis approach (#58)
    * Adding Monte Carlo sensitivity analysis approach
    
    * Complexify Monte Carlo SA tests

---
+++ R/causens_monte_carlo.R
@@ -0,0 +1,54 @@
+#' @title Monte Carlo sensitivity analysis for causal effects
+#'
+#' @description This function performs a Monte Carlo sensitivity analysis for causal effects.
+#'
+#' @param exposure The name of the exposure variable in the data frame.
+#' @param outcome The name of the outcome variable in the data frame.
+#' @param confounders The name of the confounders in the data frame.
+#' @param data A data frame containing the exposure, outcome, and confounder variables.
+#' @param ... Additional arguments to be passed to the function.
+#' @return The estimated causal effect.
+#' @export
+causens_monte_carlo <- function(exposure, outcome, confounders, data, ...) {
+  formula <- as.formula(paste(outcome, "~", exposure, "+", paste(confounders, collapse = "+")))
+
+  if (all(data[[outcome]] %in% c(0, 1))) {
+    naive_model <- glm(formula, data = data, family = binomial)
+  } else {
+    stop("Monte Carlo sensitivity analysis only supports binary outcomes, for now")
+  }
+
+  trt_effect_hat <- summary(naive_model)$coefficients["Z", "Estimate"]
+  trt_effect_sd <- summary(naive_model)$coefficients["Z", "Std. Error"]
+
+  trt_effect_corrected <- function(trt_effect) {
+    # Due to linting, the following parameters are abbreviated as follows:
+    # bU: beta_U
+    # g0: gamma_0
+    # gX: gamma_X
+    bU <- runif(1, -2, -2)
+    g0 <- runif(1, -5, 5)
+    gX <- runif(1, -2, 2)
+
+    return(trt_effect - log((1 + exp(bU + g0 + gX)) * (1 + exp(g0))) + log((1 + exp(bU + g0)) * (1 + exp(g0 + gX))))
+  }
+
+  args <- list(...)
+
+  if ("nsamples" %in% names(args)) {
+    nsamples <- args$nsamples
+  } else {
+    nsamples <- 20000
+  }
+
+  mc_trt_effects <- c()
+
+  for (i in 1:nsamples) {
+    mc_trt_effects <- c(
+      mc_trt_effects,
+      rnorm(1, trt_effect_corrected(trt_effect_hat), trt_effect_sd)
+    )
+  }
+
+  return(mean(mc_trt_effects))
+}
---
+++ tests/testthat/test_causens_monte_carlo.R
@@ -0,0 +1,33 @@
+parameters <- list(
+  list(trt_effect = 1, alpha_uz = 0.5, beta_uy = 0.2),
+  list(trt_effect = 1, alpha_uz = -0.5, beta_uy = 0.2),
+  list(trt_effect = 1, alpha_uz = 0.5, beta_uy = -0.2),
+  list(trt_effect = 1, alpha_uz = -0.5, beta_uy = -0.2),
+  list(trt_effect = 2, alpha_uz = 0.5, beta_uy = 0.2)
+)
+
+for (params in parameters) {
+  trt_effect <- params$trt_effect
+  alpha_uz <- params$alpha_uz
+  beta_uy <- params$beta_uy
+
+  run_simulation <- function(seed) {
+    data <- simulate_data(
+      N = 1000, alpha_uz = alpha_uz, beta_uy = beta_uy,
+      seed = seed, treatment_effects = trt_effect, y_type = "binary",
+      informative_u = FALSE
+    )
+
+    return(causens(Z ~ X.1 + X.2 + X.3, "Y", data, method = "Monte Carlo"))
+  }
+
+  simulated_ate <- c()
+
+  for (seed in 1:100) {
+    simulated_ate <- c(simulated_ate, run_simulation(seed))
+  }
+
+  test_that("Monte Carlo method returns correct ATE", {
+    expect_equal(trt_effect, mean(simulated_ate), tolerance = 0.05)
+  })
+}
